507237e31985625b96902b573e3e7757a58a4125
Igor Anzheyevskiy Zakharyk <igor_lifeking@hotmail.com>
2024-10-11 11:28:06 +0000

    feat: added guessing game

--- number_guess.sh
+++ number_guess.sh
@@ -4,6 +4,27 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 MAX=1000
 MIN=1
 RANDOM_NUMBER=$((RANDOM % (MAX - MIN + 1) + MIN))
+ATTEMPTS=0
+
+GUESS_GAME (){
+  echo -e "$1"
+  read GUESS
+
+  if [[  $GUESS =~ ^[0-9]+$  ]]; then
+    if [[  $GUESS > $RANDOM_NUMBER  ]]; then
+    GUESS_GAME "It's lower than that, guess again:"
+    let COUNT=COUNT+1
+    elif [[ $GUESS < $RANDOM_NUMBER  ]]; then
+    GUESS_GAME "It's higher than that, guess again:"
+    let COUNT=COUNT+1
+    elif [[  $GUESS ==  $RANDOM_NUMBER  ]]; then
+    You guessed it in $ATTEMPTS tries. The secret number was $RANDOM_NUMBER. Nice job!
+    fi
+  else
+    GUESS_GAME "That is not an integer, guess again:"
+  fi
+
+}
 
 echo -e "Enter your username:\n"
 read USERNAME
