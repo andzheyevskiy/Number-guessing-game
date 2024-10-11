35899cef6e5afcd98d240c52216ad212abbaf5c9
Igor Anzheyevskiy Zakharyk <igor_lifeking@hotmail.com>
2024-10-11 11:17:55 +0000

    feat: added greeting

--- number_guess.sh
+++ number_guess.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 MAX=1000
 MIN=1
 RANDOM_NUMBER=$((RANDOM % (MAX - MIN + 1) + MIN))
 
+echo -e "Enter your username:\n"
+read USERNAME
+
+USER_DATA=$($PSQL "SELECT id, name, games_played, best_game FROM username WHERE name='$USERNAME'")
+
+if [[  -z $USER_DATA  ]]; then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+
+else
+  echo $USER_DATA | while read ID BAR NAME BAR GAMES_PLAYED BAR BEST_GAME
+    do
+    echo -e "Welcome back, $NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+    done
+fi
